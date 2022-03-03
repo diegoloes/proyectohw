44a9f18022ee08273e09701ada286b038a5dfc91
diegoloesmx <diegoloes1993@hotmail.com>
2022-03-02 19:20:22 -0600

    cambios nuevos

--- index.js
+++ index.js
@@ -1,53 +1,12 @@
-/*class car {
-    constructor(name, year, niv, milage, unit){
-        this.name = name;
-       this.year = year;
-    this.niv = niv;
-        this.milage = milage;
-        this.unit = unit;
+.document
 
-    }
-    info() {
-       console.log(`ste coche ${this.name} da ${this.milage} ${this.unit}`)
+(function()
+{
+    var agregar = function(){};
+    var boton = document.getElementById('boton');
+    boton.addEventListener("click", agregado);
+})
 
-    }
-}
-const ecosport = new car ( "ford", 2017, "qwqwekjhdfd","15","km/l");
-
-ecosport.info();
-
-
-//const test = Array.from(Array)(1000).keys());
-//test.array.forEach(Function(E) {
-  //  if (e === 10) {
-     //   return
-    //}
-    //console.log(e)
-    
-//})
-
-*/
-//class vec {
-  //  constructor(x, y) {
-    //    this.x = x;
-    //}
-    //plus(other) { return new vec(this.x + other.x, this.y + other.y) }
-    //minus(other) { return new vec(this.x - other.x, this.y - other.y) }
-    //length(other) { return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2)) }
-//}
-//var vec1 = new vec(1, 2);
-//var vec2 = new vec(2, 3);
-//console.log(vec1.plus(vec2)); // Vec { x: 3, y: 5 }
-//console.log(vec1.minus(vec2)); // Vec { x: -1, y: -1 }
-//console.log(vec1.length()); // 2.23606797749979 */
-const bloque = document.querySelector(".bloque");
-    bloque.addEventListener("mouseeter", () =>  {
-        bloque.getElementsByClassName.backgoundcolor = "blue";
-    });
-    
-    bloque.addEventListener("mouseeter", () =>  {
-        bloque.getElementsByClassName.backgoundcolor = "red";
-    });
     
 
 
